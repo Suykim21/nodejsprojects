f34e7eccd48c6016a6bb5bcbdc45a25657a55a34
Steve Kim <suykim21@Steves-MacBook.local>
2018-06-27 22:15:27 -0700

    worked on validations login/reg

--- vidjot/app.js
+++ vidjot/app.js
@@ -1,36 +1,46 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const path = require('path');
+const exphbs  = require('express-handlebars');
 const methodOverride = require('method-override');
-const session = require('express-session');
 const flash = require('connect-flash');
-const app = express();
-const port = 5000;
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const mongoose = require('mongoose');
 
+const app = express();
+
+// Load routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+// Passport Config
+//require('./config/passport')(passport);
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
 // Connect to mongoose
 mongoose.connect('mongodb://localhost/vidjot-dev')
-  .then(() => console.log('MongoDB connected...'))
+  .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-// Load Idea Model
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
-
 // Handlebars Middleware
-app.engine('handlebars', exphbs({ //app.engine(ext, callback) 
+app.engine('handlebars', exphbs({
   defaultLayout: 'main'
-})); 
-app.set('view engine', 'handlebars'); //app.set(name, value)
+}));
+app.set('view engine', 'handlebars');
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Method override middleware
 app.use(methodOverride('_method'));
 
-// Express Session middleware
+// Express session midleware
 app.use(session({
   secret: 'secret',
   resave: true,
@@ -39,8 +49,8 @@ app.use(session({
 
 app.use(flash());
 
-// Global Variables
-app.use((req, res, next) => {
+// Global variables
+app.use(function(req, res, next){
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
@@ -48,9 +58,9 @@ app.use((req, res, next) => {
 });
 
 // Index Route
-app.get('/', (req, res) => { // app.get(path, callback [, callback ...])
+app.get('/', (req, res) => {
   const title = 'Welcome';
-  res.render('index',  {
+  res.render('index', {
     title: title
   });
 });
@@ -60,89 +70,13 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-// Add Idea Form
-app.get('/ideas/add', (req, res) => {
-  res.render('ideas/add');
-});
 
-// Edit Idea Form
-app.get('/ideas/edit/:id', (req, res) => {
-  Idea.findOne({
-    _id: req.params.id
-  })
-  .then(idea => {
-    res.render('ideas/edit', {
-      idea:idea
-    })
-  });
-});
+// Use routes
+app.use('/ideas', ideas);
+app.use('/users', users);
 
-// Idea Index Page
-app.get('/ideas', (req, res) => {
-  Idea.find({})
-    .sort({date:'desc'}) // sorting by descending order
-    .then(ideas => {
-      res.render('ideas/index', {
-        ideas:ideas
-      });
-    });
-});
+const port = 5000;
 
-// Process Form
-app.post('/ideas', (req, res) => {
-  let errors = [];
-  if(!req.body.title){
-    errors.push({text: 'Please add a title'});
-  }
-  if(!req.body.details) {
-    errors.push({text:'Please add some details'});
-  }
-  if(errors.length > 0) {
-    res.render('ideas/add', {
-      errors: errors,
-      title: req.body.title,
-      details: req.baseUrl.details
-    })
-  } else {
-    const newUser = {
-      title: req.body.title,
-      details: req.body.details
-    }
-    new Idea(newUser)
-      .save()
-      .then(idea => {
-        req.flash('success_msg', 'Video idea added');
-        res.redirect('/ideas');
-      })
-  }
-});
-
-// Edit Form Process
-app.put('/ideas/:id', (req, res) => {
-  Idea.findOne({
-    _id: req.params.id
-  })
-  .then(idea => {
-    // new values
-    idea.title = req.body.title;
-    idea.details = req.body.details;
-    idea.save()
-      .then(idea => {
-        req.flash('success_msg', 'Video idea updated');
-        res.redirect('/ideas');
-      })
-  });
-});;
-
-// Delete Idea
-app.delete('/ideas/:id', (req, res) => {
-  Idea.remove({_id: req.params.id})
-    .then(() => {
-      req.flash('success_msg', 'Video idea removed');
-      res.redirect('/ideas');
-    });
-});
-
-app.listen(port, () => { //app.listen(path, [callback])
-  console.log(`Server running at ${port}`);
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`);
 });
